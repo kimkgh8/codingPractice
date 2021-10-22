016d97994bfbe3e417c3a59a795bc2a779ca1b5a
KimGuenho <kimkgh8@gmail.com>
2021-10-22 18:07:06 +0900

    상하좌우 (나의방법) 범위에 대한 판단은 되지 않은 버전

---
+++ codingPractice/src/personal/practice_Implementation_MyWay.java
@@ -0,0 +1,49 @@
+package personal;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+
+//1이 될 때까지
+public class practice_Implementation_MyWay {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(br.readLine());
+		int x = 1;
+		int y = 1;
+		
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		for (int i=0;i<st.countTokens();i++) {
+			String moveLocation = st.nextToken();
+			System.out.println("moveLocation:"+moveLocation);
+			if (moveLocation != null && !moveLocation.isEmpty()) {
+				String val = move(moveLocation,x,y);
+				x = Integer.parseInt(val.substring(0,val.indexOf(",")));
+				y = Integer.parseInt(val.substring(val.indexOf(",")+1,val.length()));
+			} else {
+				break;
+			}
+		}
+		
+		System.out.println(x+" "+y);
+		
+	}
+	
+	public static String move (String moveLocation, int x, int y ) {
+		switch (moveLocation) {
+			case "L":
+				y -= 1;
+			case "R":
+				y += 1;
+			case "U":
+				x -= 1;
+			case "D":
+				x += 1;
+		}
+		String movement = String.valueOf(x)+","+String.valueOf(y);
+		return movement;
+	}
+
+}
